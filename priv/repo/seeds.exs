ac46160dd092b4081215e942eb1b0cb7b3ba3150
Mpreyzner <maria.preyzner@gmail.com>
2017-10-07 18:57:20 +0200

    add content to seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Bussy.Repo.delete_all Bussy.User
+
+Bussy.User.changeset(%Bussy.User{}, %{name: "Test User", email: "testuser@example.com", password: "secret", password_confirmation: "secret"})
+|> Bussy.Repo.insert!
