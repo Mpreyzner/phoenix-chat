9f828e459c133a06bf53e0b2f813ddbf275dff20
Mpreyzner <maria.preyzner@gmail.com>
2017-10-07 19:15:58 +0200

    fix routing

--- web/router.ex
+++ web/router.ex
@@ -11,8 +11,28 @@ defmodule Bussy.Router do
     plug Coherence.Authentication.Session
   end
 
+    
+pipeline :protected do
+  plug :accepts, ["html"]
+  plug :fetch_session
+  plug :fetch_flash
+  plug :protect_from_forgery
+  plug :put_secure_browser_headers
+  plug Coherence.Authentication.Session, protected: true
+end
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/" do
+    pipe_through :browser
+    coherence_routes()
+  end
+
+  scope "/" do
+    pipe_through :protected
+    coherence_routes :protected
   end
 
   scope "/", Bussy do
