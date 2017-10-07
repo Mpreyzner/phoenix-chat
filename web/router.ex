8cddc1ff96eea51c8995fa607ba9b0d9d2d27809
Mpreyzner <maria.preyzner@gmail.com>
2017-10-08 00:13:21 +0200

    add user recognition from token

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,7 @@ defmodule Bussy.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug Coherence.Authentication.Session, protected: true
+    plug :put_user_token
   end
 
   pipeline :api do
@@ -35,14 +36,19 @@ defmodule Bussy.Router do
     coherence_routes :protected
   end
 
-  # scope "/", Bussy do
-  #   pipe_through :browser # Use the default browser stack
-
-  #   get "/", PageController, :index
-  # end
  scope "/", Bussy do
   pipe_through :protected
     get "/", PageController, :index
+ end
+
+ defp put_user_token(conn, _) do
+   current_user = Coherence.current_user(conn).id
+   user_id_token = Phoenix.Token.sign(conn,
+    "user_id",
+    Coherence.current_user(conn).id)
+
+   conn
+   |> assign(:user_id, user_id_token)
  end
  
 end
